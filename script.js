596bc2a45a2d15c677da04e004822646469fbbc8
umairansari92 <154614381+umairansari92@users.noreply.github.com>
2024-02-18 17:32:26 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-// // Toggle dark mode
-// const btnToggle = document.querySelector('.btn-toggle');
-// const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
-// const currentTheme = localStorage.getItem('theme') || 'light';
+// Toggle dark mode
+const btnToggle = document.querySelector('.btn-toggle');
+const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
+const currentTheme = localStorage.getItem('theme') || 'light';
 
-// if (currentTheme === 'dark') {
-//     document.body.classList.add('dark-mode');
-// } else {
-//     document.body.classList.remove('dark-mode');
-// }
+if (currentTheme === 'dark') {
+    document.body.classList.add('dark-mode');
+} else {
+    document.body.classList.remove('dark-mode');
+}
 
-// btnToggle.addEventListener('click', () => {
-//     const theme = document.body.classList.contains('dark-mode') ? 'light' : 'dark';
-//     document.body.classList.toggle('dark-mode');
-//     localStorage.setItem('theme', theme);
-// });
+btnToggle.addEventListener('click', () => {
+    const theme = document.body.classList.contains('dark-mode') ? 'light' : 'dark';
+    document.body.classList.toggle('dark-mode');
+    localStorage.setItem('theme', theme);
+});
